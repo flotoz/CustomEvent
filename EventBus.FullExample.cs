1e9af3e62ec8d4089ab8b2fb11f054229e613289
Flotoz <46895887+flotoz@users.noreply.github.com>
2025-04-27 20:40:41 +0200

    chore: register all handlers when backgroundservice starts

--- EventBus.FullExample.cs
+++ EventBus.FullExample.cs
@@ -1,62 +1,90 @@
+using System.Threading.Channels;
+
 namespace Medium.Examples;
 
 public interface IEvent { }
-public interface IEventHandler<in TEvent> where TEvent : IEvent
+
+public interface IEventHandler
 {
-    Task HandleAsync(TEvent @event, CancellationToken cancellationToken = default);
+    void Register(IEventBus eventBus);
+}
+
+public interface IEventHandler<in TEvent> : IEventHandler
+    where TEvent : IEvent
+{
+    Task HandleAsync(TEvent integrationEvent, CancellationToken cancellationToken = default);
 }
 
 public interface ICommandEvent { }
-public interface ICommandEventHandler<in TCommand>
+public interface ICommandEventHandler<in TCommand> : IEventHandler
     where TCommand : ICommandEvent
 {
     Task HandleAsync(TCommand @event, CancellationToken cancellationToken = default);
 }
 
 public interface IQueryEvent { }
-public interface IQueryEventHandler<in TQuery, TResult>
+public interface IQueryEventHandler<in TQuery, TResult> : IEventHandler
     where TQuery : IQueryEvent
 {
     Task<TResult> HandleAsync(TQuery @event, CancellationToken cancellationToken = default);
 }
 
+internal sealed class InMemoryMessageQueue
+{
+    private readonly Channel<IEvent> _channel = 
+        Channel.CreateUnbounded<IEvent>();
+
+    public ChannelReader<IEvent> Reader => _channel.Reader;
+    public ChannelWriter<IEvent> Writer => _channel.Writer;
+}
 
 public interface IEventBus
 {
-    // Register Command Handlers
-    void RegisterCommandHandler<TCommand>(ICommandEventHandler<TCommand> handler)
+    Task StartConsumingAsync(CancellationToken cancellationToken = default);
+    
+    void Register<TCommand>(ICommandEventHandler<TCommand> handler)
         where TCommand : ICommandEvent;
 
-    // Register Query Handlers
-    void RegisterQueryHandler<TQuery, TResult>(IQueryEventHandler<TQuery, TResult> handler)
+    void Register<TQuery, TResult>(IQueryEventHandler<TQuery, TResult> handler)
         where TQuery : IQueryEvent;
 
-    // Publish Event Handlers
-    void RegisterEventHandler<TEvent>(IEventHandler<TEvent> handler)
+    void Register<TEvent>(IEventHandler<TEvent> handler)
         where TEvent : IEvent;
 
-    // Send Command (Async)
     Task SendCommandAsync<TCommand>(TCommand commandEvent, CancellationToken cancellationToken = default)
         where TCommand : ICommandEvent;
 
-    // Send Query (Async) and return result
     Task<TResult> SendQueryAsync<TQuery, TResult>(TQuery queryEvent, CancellationToken cancellationToken = default)
         where TQuery : IQueryEvent;
 
-    // Publish Event (Async)
     Task PublishAsync<TEvent>(TEvent eventToPublish, CancellationToken cancellationToken = default)
         where TEvent : IEvent;
 }
 
-
-internal sealed class EventBus : IEventBus
+internal sealed class EventBus(InMemoryMessageQueue _queue) : IEventBus
 {
     private readonly Dictionary<Type, Func<ICommandEvent, CancellationToken, Task>> _commandHandlers = new();
     private readonly Dictionary<Type, Func<IQueryEvent, CancellationToken, Task<object>>> _queryHandlers = new();
     private readonly Dictionary<Type, List<Func<IEvent, CancellationToken, Task>>> _eventHandlers = new();
 
+    public async Task StartConsumingAsync(CancellationToken cancellationToken = default)
+    {
+        await foreach (var integrationEvent in _queue.Reader.ReadAllAsync(cancellationToken))
+        {
+            if (!_eventHandlers.TryGetValue(integrationEvent.GetType(), out var handlers))
+            {
+                continue;
+            }
+
+            foreach (var handler in handlers)
+            {
+                await handler(integrationEvent, cancellationToken);
+            }
+        }
+    }
+    
     // Register Command Handlers
-    public void RegisterCommandHandler<TCommand>(ICommandEventHandler<TCommand> handler)
+    public void Register<TCommand>(ICommandEventHandler<TCommand> handler)
         where TCommand : ICommandEvent
     {
         var commandType = typeof(TCommand);
@@ -67,72 +95,88 @@ internal sealed class EventBus : IEventBus
     }
 
     // Register Query Handlers
-    public void RegisterQueryHandler<TQuery, TResult>(IQueryEventHandler<TQuery, TResult> handler)
+    public void Register<TQuery, TResult>(IQueryEventHandler<TQuery, TResult> handler)
         where TQuery : IQueryEvent
     {
         var queryType = typeof(TQuery);
-        _queryHandlers[queryType] = async (query, token) =>
-        {
-            return await handler.HandleAsync((TQuery)query, token);
-        };
+        _queryHandlers[queryType] = async (query, token) => await handler.HandleAsync((TQuery)query, token);
     }
 
     // Register Event Handlers (for Publishing Events)
-    public void RegisterEventHandler<TEvent>(IEventHandler<TEvent> handler)
+    public void Register<TEvent>(IEventHandler<TEvent> handler)
         where TEvent : IEvent
     {
         var eventType = typeof(TEvent);
         if (!_eventHandlers.ContainsKey(eventType))
         {
-            _eventHandlers[eventType] = new List<Func<IEvent, CancellationToken, Task>>();
+            _eventHandlers[eventType] = new();
         }
 
-        _eventHandlers[eventType].Add(async (eventToHandle, token) =>
-        {
-            await handler.HandleAsync((TEvent)eventToHandle, token);
-        });
+        _eventHandlers[eventType].Add(async (eventToHandle, token) => await handler.HandleAsync((TEvent)eventToHandle, token));
     }
 
     public async Task SendCommandAsync<TCommand>(TCommand commandEvent, CancellationToken cancellationToken = default)
         where TCommand : ICommandEvent
     {
-        if (_commandHandlers.TryGetValue(commandEvent.GetType(), out var handler))
-        {
-            await handler(commandEvent, cancellationToken);
-        }
-        else
+        if (!_commandHandlers.TryGetValue(commandEvent.GetType(), out var handler))
         {
             throw new ArgumentException($"No handler registered for command '{commandEvent.GetType().Name}'");
-        }
+        }  
+        await handler(commandEvent, cancellationToken);
     }
 
     public async Task<TResult> SendQueryAsync<TQuery, TResult>(TQuery queryEvent, CancellationToken cancellationToken = default)
         where TQuery : IQueryEvent
     {
-        if (_queryHandlers.TryGetValue(queryEvent.GetType(), out var handler))
-        {
-            return (TResult)await handler(queryEvent, cancellationToken);
+        if (!_queryHandlers.TryGetValue(queryEvent.GetType(), out var handler))
+        { 
+            throw new ArgumentException($"No handler registered for query '{queryEvent.GetType().Name}'"); 
         }
-        else
-        {
-            throw new ArgumentException($"No handler registered for query '{queryEvent.GetType().Name}'");
-        }
+        return (TResult)await handler(queryEvent, cancellationToken);
     }
 
     public async Task PublishAsync<TEvent>(TEvent eventToPublish, CancellationToken cancellationToken = default)
         where TEvent : IEvent
     {
         var eventType = typeof(TEvent);
-        if (_eventHandlers.TryGetValue(eventType, out var handlers))
-        {
-            foreach (var handler in handlers)
-            {
-                await handler(eventToPublish, cancellationToken);
-            }
-        }
-        else
+        if (!_eventHandlers.TryGetValue(eventType, out var handlers))
         {
             throw new ArgumentException($"No handler registered for event '{eventToPublish.GetType().Name}'");
         }
+        foreach (var handler in handlers)
+        {
+            await handler(eventToPublish, cancellationToken);
+        }
+    }
+}
+
+public class EventBusBackgroundService(IEventBus _eventBus, IServiceProvider _serviceProvider) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using var scope = _serviceProvider.CreateScope();
+        
+        var eventHandlers = scope.ServiceProvider.GetServices<IEventHandler>();
+        
+        foreach (var eventHandler in eventHandlers)
+        {
+            eventHandler.Register(_eventBus);
+        }
+        await _eventBus.StartConsumingAsync(stoppingToken);
+    }
+}
+
+public class UserCreatedHandler : IEventHandler<UserCreatedHandler.UserCreatedEvent>
+{
+    public record UserCreatedEvent(Guid UserId) : IEvent;
+    public void Register(IEventBus eventBus)
+    {
+        eventBus.Register(this);
+    }
+
+    public Task HandleAsync(UserCreatedEvent @event, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"User created: {@event.UserId}");
+        return Task.CompletedTask;
     }
 }
